a54216451423bae830be3bfc6ba1d4ca9feb9652
unknown <bimarshkhatri123@gmail.com>
2023-06-03 17:58:20 +0800

    New

---
+++ app/src/main/java/hdu/course/chinese_food/MainActivity.java
@@ -0,0 +1,11 @@
+package hdu.course.chinese_food;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+         setContentView(R.layout.dim_sum2);
+    }
+}
